34733aba5bc6ac5aff62a92358008eadfc96120e
unknown <singhrathoreayush824@gmail.com>
2025-10-05 11:39:41 +0530

    week 2 Alchemy builder

--- client/index.js
+++ client/index.js
@@ -2,16 +2,22 @@ const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = 'http://localhost:5000'; // same port as server
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const name = 'S Block'; // change this to test different names
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+
+  console.log("📦 Sending proof and name to server...");
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof,
   });
 
-  console.log({ gift });
+  console.log("🎁 Server response:", gift);
 }
 
 main();
--- server/index.js
+++ server/index.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
 
-const port = 1225;
-
+const port = 5000; // server port
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
+// Hardcode the Merkle root
 const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+  if (isInTheList) {
+    res.send("🎁 You got a toy robot!");
+  } else {
+    res.send("❌ You are not on the list :(");
   }
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+  console.log(`✅ Server listening on port ${port}!`);
 });
